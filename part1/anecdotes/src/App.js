a00f96749aed6fed12d6550797326e0f42d25a55
jroberto37 <joseroberto37@hotmail.com>
2022-07-02 15:34:39 -0500

    Part 1 complete

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+
+const getRandom = (min, max) => Math.floor( Math.random() * (max - min) + min );
+
+const Button = ( { handleFunction, text } ) => {
+
+  return(
+    <>
+      <button onClick = { handleFunction } > { text } </button>
+    </>
+  )
+
+}
+
 
 function App() {
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+
+  const [ selected, setSelected ] = useState(0); 
+  const [ votes, setVotes ] = useState( new Array(anecdotes.length).fill(0));
+
+  const sumVote = () => {
+    return votes.map( function(x, conta) {
+        if (conta === selected){
+          return x + 1
+        }
+        return x
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>{ anecdotes[selected] }</p>
+      <p> has { votes[selected] } votes</p>
+      <Button handleFunction={ () => setVotes( sumVote  ) } text="vote" />
+      <Button handleFunction={ () => setSelected(getRandom(0,7)) } text="next anecdote"/>
+      <h2>Anecdote with most votes</h2>
+      <p>{ anecdotes[ votes.indexOf(Math.max(...votes)) ] }</p>
+
+    </>
   );
 }
 
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import './app.css'
 
-function App() {
+const Button = ({functionEvent, text}) => {
+  return(
+    <>
+      <button onClick={ functionEvent } >{text}</button>
+    </>
+  )
+}
+
+const Message = ({feedback, statics}) => {
+  if (statics.all > 0) {
+    return(
+      <>
+        <h2>Statics</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td scope="row">good</td>
+              <td>{ feedback.good }</td>              
+            </tr>
+            <tr>
+              <td scope="row">neutrals </td>
+              <td>{ feedback.neutral }</td>
+            </tr>
+            <tr>
+              <td scope="row">bad</td>
+              <td>{ feedback.bad }</td>
+            </tr>            
+          </tbody>
+        </table>
+
+      </>
+    )
+  }else{
+    return(<h3>No feedback given</h3>)
+  }
+}
+
+const Static = ({feedback, statics}) => {
+  if (statics.all > 0 ){
+    return(
+      <>
+        <table className="table">
+          <tbody>
+            <tr>
+              <td scope="row">all</td>
+              <td>{ statics.all }</td>
+            </tr>
+            <tr>
+              <td scope="row">average</td>
+              <td>{ isNaN( (( statics.positives - statics.negatives ) / statics.all) ) ? 0 : (( statics.positives - statics.negatives ) / statics.all) }</td>
+            </tr>
+            <tr>
+              <td scope="row">positive</td>
+              <td>{ isNaN( feedback.good / statics.all ) ? 0 : (feedback.good / statics.all) * 100 }</td>
+            </tr>            
+          </tbody>
+        </table>
+      </>
+    )
+  }
+}
+
+const App = () =>  {
+  const message = 'Give feedback'
+  const [feedback, setFeedBack] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  })
+
+  const [statics, setStatics] = useState({
+    all: 0,
+    positives: 0,
+    negatives: 0
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>{ message }</h1>
+      <Button functionEvent = { 
+        () => {
+          
+          setFeedBack({ good: feedback.good += 1, ...feedback })
+          setStatics({ all: statics.all += 1, positives: statics.positives += 1, ...statics })
+        
+        }} text="good" />
+      <Button functionEvent = { () => {
+
+          setFeedBack( { neutral: feedback.neutral += 1, ...feedback } ) 
+          setStatics(  { all: statics.all += 1, ...statics} )
+
+        }} text="neutral" /> 
+      <Button functionEvent = { () => {
+
+          setFeedBack( { bad: feedback.bad += 1, ...feedback } ) 
+          setStatics( { all: statics.all += 1, negatives: statics.negatives += 1, ...statics } )
+
+        }} text="bad" />
+      
+      <Message feedback = { feedback } statics = { statics } />
+      <Static feedback = { feedback } statics = { statics } />
+      
+    </>
+  )
 }
 
 export default App;
